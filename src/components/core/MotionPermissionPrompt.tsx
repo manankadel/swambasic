37a927f74f9b08e977461aed40b728a03ce70f0d
manankadel <manankadel@gmail.com>
2025-07-28 03:54:40 +0530

    Gyro

--- src/components/core/MotionPermissionPrompt.tsx
+++ src/components/core/MotionPermissionPrompt.tsx
@@ -52,7 +52,7 @@ export const MotionPermissionPrompt = () => {
     }
 
     return (
-        <div className="fixed bottom-4 right-4 z-50 flex items-center gap-3">
+        <div className="fixed bottom-4 left-4 z-50 flex items-center gap-3">
             <AnimatePresence>
                 {showHint && (
                     <motion.div
